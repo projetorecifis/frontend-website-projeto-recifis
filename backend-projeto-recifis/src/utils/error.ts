87c8eb8bea1d1c3e6e28028b7de42382fc11981e
Tássia Martins <tassia.gomes@viaconsulting.com.br>
2025-03-26 13:56:02 -0300

    feat: migrating backend files to another repository

--- backend-projeto-recifis/src/utils/error.ts
+++
@@ -1,8 +0,0 @@
-export function getErrorMessage(error: unknown) {
-	if (error instanceof Error) return error.message
-	return String(error)
-}
-
-export const reportError = ({ message }: { message: string }) => {
-	// send the error to our logging service...
-}
