5ed66d7fd098c556ec64bc99d3dd6aa01d6667e3
Projeto Recifis <projetorecifis@gmail.com>
2025-04-15 17:03:31 -0300

    feat: changing podcasts page

--- src/app/(main-website)/conteudos/podcasts/(components)/show-podcasts.tsx
+++ src/app/(main-website)/conteudos/podcasts/(components)/show-podcasts.tsx
@@ -2,7 +2,7 @@
 
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
-import { Play, CirclePlay, Mic } from "lucide-react";
+import { Play, CirclePlay, Mic, Podcast } from "lucide-react";
 import { BaselinePlayCircle, PlaySkipBackSharp, PlaySkipForwardSharp } from "./icon";
 import {
     Accordion,
@@ -13,6 +13,9 @@ import {
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { useState } from "react";
+import { Separator } from "@/components/ui/separator";
+import { ChangeTheme } from "@/components/created/ChangeTheme";
+
 
 
 const podcasts = [
@@ -80,29 +83,38 @@ export default function ShowPodcasts() {
         setCurrentImage(image);
     }   
     return (
-        <section className="m-auto w-456 flex flex-row-reverse justify-center max-h-dvh items-center rounded-lg pb-16 gap-16">
-            <div className="w-1/2">
+        <section className="m-auto  flex flex-col justify-center items-center rounded-lg tabl:flex-row tabl:max-h-dvh desk:w-456 desk:gap-16">
+            <div className="tabl:px-4 desk:px-0">
                 {currentImage && (
                     <Image 
                         src={currentImage}
-                        alt="Logo do Recifis"
-                        width={380}
-                        height={380}
+                        alt=""
+                        width={480}
+                        height={480}
                         className="rounded-lg"
                     />
                 )}
                 {!currentImage && (
-                    <Card className="w-96 h-96 bg-slate-300 dark:bg-slate-600 dark:border-none">
+                    <Card className="w-80 h-80 bg-slate-300 dark:bg-slate-600 dark:border-none">
                         <CardContent className="flex items-center justify-center">
-                            <Mic className="text-center mt-32 text-slate-800" size={150} />
+                            <Mic className="text-center mt-24 text-slate-800" size={120} />
                         </CardContent>
                     </Card>
                 )}
+                 <div className="flex items-center justify-center gap-2 py-4">
+                    <p>Tente trocar o tema:</p>
+                    <ChangeTheme />
+                </div>
             </div>
 
-           <div className="w-full rounded-lg scroll-smooth bg-slate-200 dark:bg-gray-900">
-                <ScrollArea className="h-screen rounded-md">
-                    <Accordion type="single" collapsible className="w-full border-2 border-gray-300 rounded-lg dark:text-slate-300 dark:border-none">
+           <div className="w-full rounded-lg scroll-smoothpb-8 dark:bg-gray-900 ">
+            
+                <div className="flex items-center justify-center py-4 text-center text-sm tabl:text-basic tabl:gap-2 dark:bg-slate-900">
+                    <Podcast className="mx-4 tabl:mx-0" />
+                    <h1>Veja a lista de podcasts que preparamos especialmente para você:</h1>
+                </div>
+                <ScrollArea className="rounded-md tabl:h-screen">
+                    <Accordion type="single" collapsible className="w-full border-2 rounded-lg dark:text-slate-300 dark:border-none max-h-112  ">
                     {podcasts?.map((podcast, index) => (
                         <AccordionItem key={index} value={`item-${index}`} className=" rounded-md border-slate-500 hover:bg-slate-100 dark:hover:bg-slate-800 dark:border-slate-600">
                             <AccordionTrigger 
@@ -118,7 +130,7 @@ export default function ShowPodcasts() {
                                     className="rounded-lg"
                                 />
                                 <div className="text-start">
-                                    <h1 className="font-bold text-xl dark:text-slate-300">{podcast?.title}</h1>
+                                    <h1 className="font-bold text-md tabl:text-xl dark:text-slate-300">{podcast?.title}</h1>
                                     <p className="dark:text-gray-400">{podcast?.littleDescription}</p>
                                 </div>
                             </AccordionTrigger>
--- src/app/(main-website)/conteudos/podcasts/page.tsx
+++ src/app/(main-website)/conteudos/podcasts/page.tsx
@@ -1,15 +1,18 @@
 import { MicVocal } from "lucide-react";
 import ShowPodcasts from "./(components)/show-podcasts";
+import { Separator } from "@/components/ui/separator";
 
 export default function Content() {
     return (
-        <main className="max:h-full bg-slate-200 dark:bg-slate-950">
-            <div className=" text-slate-900 flex items-center justify-center gap-4 pt-10 pb-16">
+        <main className="max:h-full bg-white dark:bg-slate-950 pb-8 tabl:pb-20">
+            <div className=" text-slate-900 flex items-center justify-center gap-4 pt-6 dark:text-slate-100">
                 <MicVocal className="h-8 w-8" />
-                <h1 className="uppercase text-center text-3xl font-bold">
+                <h1 className="uppercase text-center text-3xl font-bold ">
                     Podcasts 
                 </h1>
             </div>
+            <Separator className="my-6 tabl:mb-12 dark:mb-16" />
+
             <ShowPodcasts />
         </main>
     );
